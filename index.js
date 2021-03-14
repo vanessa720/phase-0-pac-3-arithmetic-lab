c89b84570a2c63cc42f288a7c51981d9f63a058a
Vanessa720 <vanessawilliamson718@gmail.com>
2021-03-13 22:15:49 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,33 @@
+
+
+function subtract(a,b) {
+  return a - b
+}
+
+function multiply (a,b) {
+  return a * b
+}
+
+function add (a,b) {
+  return a + b 
+}
+
+function divide (a,b) {
+  return a / b; //= 1.224
+}
+  
+function increment (n) {
+  return ++(n)
+}
+
+function decrement (n) {
+  return --(n)
+}
+
+function makeInt(string) {
+  return parseInt(string, 10)
+}
+
+function preserveDecimal(string) {
+  return parseFloat(string)
+}
